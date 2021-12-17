c525714447c9bd002db22165e6fb2950f554aeea
Logan McAnsh <logan@mcan.sh>
2021-12-17 12:49:11 -0500

    initial commit
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ my-remix-app/server/index.js
@@ -0,0 +1,73 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs/promises");
+const { constants } = require("fs");
+
+const { createRequestHandler } = require("../../dist/src");
+
+async function checkFileExists(filepath) {
+  try {
+    let file = await fs.stat(filepath, constants.F_OK);
+    return file.isFile();
+  } catch (error) {
+    return false;
+  }
+}
+
+async function serveFile(req, res) {
+  const filePath = path.join(process.cwd(), "public", req.url);
+  const fileExists = await checkFileExists(filePath);
+  if (!fileExists) return undefined;
+  return await fs.readFile(filePath, "utf-8");
+}
+
+const MODE = process.env.NODE_ENV;
+const BUILD_DIR = path.join(process.cwd(), "server/build");
+
+let server = http.createServer(async (req, res) => {
+  try {
+    let file = await serveFile(req, res);
+    if (file) {
+      return res.end(file);
+    }
+    purgeRequireCache();
+    let build = require("./build");
+    createRequestHandler({ build, mode: MODE })(req, res);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+server.listen(process.env.PORT || 3000);
+
+// app.use(express.static("public/build", { immutable: true, maxAge: "1y" }));
+
+// app.all(
+//   "*",
+//   MODE === "production"
+//     ? createRequestHandler({ build: require("./build") })
+//     : (req, res, next) => {
+//         purgeRequireCache();
+//         let build = require("./build");
+//         return createRequestHandler({ build, mode: MODE })(req, res, next);
+//       }
+// );
+
+// let port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Express server listening on port ${port}`);
+// });
+
+////////////////////////////////////////////////////////////////////////////////
+function purgeRequireCache() {
+  // purge require cache on requests for "server side HMR" this won't let
+  // you have in-memory objects between requests in development,
+  // alternatively you can set up nodemon/pm2-dev to restart the server on
+  // file changes, we prefer the DX of this though, so we've included it
+  // for you by default
+  for (let key in require.cache) {
+    if (key.startsWith(BUILD_DIR)) {
+      delete require.cache[key];
+    }
+  }
+}
